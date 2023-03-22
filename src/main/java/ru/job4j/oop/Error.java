55b7f3386f37a1ceef97c9651c85e5f1944a9656
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-03-22 18:10:05 +0400

    1.7. Конструктор [#173889] Создайте класс Error.

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.print(active + " ");
+        System.out.print(status + " ");
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 1, "Error #1");
+        Error error3 = new Error(true, 1, "Error #2");
+        Error error4 = new Error(false, -1, "<NONE>");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+        error4.printInfo();
+    }
+}
