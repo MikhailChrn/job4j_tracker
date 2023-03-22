53cbeee5c7e312f313c1ebb903ab76f228e72a6e
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-03-22 15:50:22 +0400

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+import net.sf.saxon.functions.DocAvailable;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
