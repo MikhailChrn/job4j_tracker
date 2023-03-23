4339ed449721ff3c11fa9906036dc054a0d15bf3
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-03-23 12:16:27 +0400

    Правила Checkstyle [#504981] Устранение замечаний.

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,25 +2,22 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     private int sizeOfEmpty = 10;
-    public String surname;
-    public static final String NEW_VALUE = "";
+    private  String name;
+    private String surname;
+    private static final String NEW_VALUE = "";
 
-    void echo() {
+    Broken() {
     }
 
-    void media(Object obj) {
+    public void echo() {
+    }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
-
-    String name;
-
-    Broken() {
-
-    }
-
 }
