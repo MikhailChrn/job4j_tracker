09b271c94febb24d09864695b120bcab77107579
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-03-23 12:59:13 +0400

    1.2. Вызов метода объекта [#166085] Устранение замечания.

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -2,13 +2,20 @@ package ru.job4j.oop;
 
 public class Student {
     public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public void sing() {
         System.out.println("I believe I can fly");
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        for (int i = 0; i < 3; i++) {
-            petya.music();
-        }
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.sing();
+        petya.sing();
+        petya.sing();
     }
 }
