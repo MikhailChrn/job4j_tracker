a68162d0bc46331b44288b73110dd80eef57c916
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-03-24 07:43:05 +0400

    2.0. Рефакторинг - Расстояние между точками. [#122587] Устранение замечания.

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -12,23 +12,7 @@ public class Point {
         this.y = second;
     }
 
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
     public double distance(Point that) {
-        return sqrt(pow(this.x - that.getX(), 2) + pow(this.y - that.getY(), 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 }
