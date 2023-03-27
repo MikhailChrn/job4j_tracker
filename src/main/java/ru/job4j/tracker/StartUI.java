006cd61ddd0051b7f16c755efc1cbef632706daa
Mikhail <mikhail.cherneyev@yandex.ru>
2023-03-27 07:32:27 +0400

    8. Date. Отображение даты. [#310017] Устранение замечания.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,10 +3,9 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
     public static void main(String[] args) {
-        Item item = new Item("Obj1");
-        System.out.println(item.getCreated().format(formatter));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        Item item1 = new Item("Obj1");
+        System.out.println(item1.getCreated().format(formatter));
     }
 }
