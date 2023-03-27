3f9212743bf4af0e5d9e7862113ae10186d75ffe
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-03-27 17:28:04 +0400

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String nameOfBook;
+    private int numberOfPages;
+
+    public Book() {
+    }
+
+    public Book(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
+        numberOfPages = -1;
+    }
+
+    public Book(String nameOfBook, int numberOfPages) {
+        this.nameOfBook = nameOfBook;
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getNameOfBook() {
+        return nameOfBook;
+    }
+
+    public void setNameOfBook(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,22 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book[] books = new Book[] {
+                new Book("The Hound of the Baskervilles", 84),
+                new Book("The Old Man And The Sea"),
+                new Book("White Fang", 100),
+                new Book("Clean code", 462)
+        };
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getNameOfBook() + " " + books[i].getNumberOfPages());
+        }
+        System.out.println();
+        Book tmp = books[3];
+        books[3] = books[0];
+        books[0] = tmp;
+        for (Book book : books) {
+            System.out.println(book.getNameOfBook() + " " + book.getNumberOfPages());
+        }
+    }
+}
