fefd397e9a9ee29f11f7b48ba6d85b57fd5a1145
Mikhail <mikhail.cherneyev@yandex.ru>
2023-03-28 16:36:31 +0400

    5. Tracker - хранилище [#396] Добавьте недостающие методы в класс Tracker. В классе Tracker должны быть методы: +добавление заявок - public Item add(Item item); +получение списка всех заявок - public Item[] findAll(); +получение списка по имени - public Item[] findByName(String key); +получение заявки по id - public Item findById(int id);

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,33 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        int sizeResult = 0;
+        Item[] result = new Item[size];
+        for (int index = 0; index < result.length; index++) {
+            if (items[index] != null) {
+                result[sizeResult] = items[index];
+                sizeResult++;
+            }
+        }
+        result = Arrays.copyOf(result, sizeResult);
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        //получение списка по имени
+        int sizeResult = 0;
+        Item[] result = new Item[size];
+        for (int index = 0; index < result.length; index++) {
+            if (key.equals(items[index].getName())) {
+                result[sizeResult] = items[index];
+                sizeResult++;
+            }
+        }
+        result = Arrays.copyOf(result, sizeResult);
+        return result;
     }
 
     public Item findById(int id) {
