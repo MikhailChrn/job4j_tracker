52abc2736c527bfc8f0391e72d08dbb2801ec034
Mikhail <mikhail.cherneyev@yandex.ru>
2023-03-29 11:26:11 +0400

    10. toString [#310018] В классе ru.job4j.tracker.Item сгенерируйте переопределенный метод toString().

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,7 +5,9 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        Item item1 = new Item("Obj1");
+        Item item1 = new Item(1, "Obj1");
         System.out.println(item1.getCreated().format(formatter));
+        Item item2 = new Item(2, "Obj2");
+        System.out.println(item2);
     }
 }
