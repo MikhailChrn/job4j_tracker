88dcb807baa2c33a2fa290b6bb0f6e44e856cdac
Mikhail <mikhail.cherneyev@yandex.ru>
2023-04-10 13:39:45 +0400

    1. Singleton [#94606] Реализуйте singleton и добавьте недостающие методы.

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,76 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class SingleTracker {
+    private static Tracker tracker = new Tracker();
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public static Tracker getInstance() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        int sizeResult = 0;
+        Item[] result = new Item[size];
+        for (int index = 0; index < size; index++) {
+            if (key.equals(items[index].getName())) {
+                result[sizeResult] = items[index];
+                sizeResult++;
+            }
+        }
+        return Arrays.copyOf(result, sizeResult);
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return rsl;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
+    }
+}
