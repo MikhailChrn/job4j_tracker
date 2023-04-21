fe55ec6807ce60fd459de536760f9db6d3482ef4
Mikhail <mikhail.cherneyev@yandex.ru>
2023-04-21 16:40:12 +0400

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        Map<String, String> names = new HashMap<>();
+        names.put("mikhail@mail.ru", "Mikhail Cherneyev");
+        names.put("greta@mail.sw", "Greta Thunberg");
+        names.put("tutta@mail.com", "Tutta Larson");
+        for (String key : names.keySet()) {
+            String value = names.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
