c6eb85d9e9049920c8fbea4a239edd5e4f19646c
Mikhail <mikhail.cherneyev@yandex.ru>
2023-04-23 13:47:37 +0400

    0. Map, HashMap [#212652 #411365] Устранение замечания.

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,8 @@ public class UsageMap {
     public static void main(String[] args) {
         Map<String, String> names = new HashMap<>();
         names.put("mikhail@mail.ru", "Mikhail Cherneyev");
+        names.put("greta@mail.sw", "Igor Gretovich");
+        names.put("tutta@mail.com", "Daniel Tittovich");
         names.put("greta@mail.sw", "Greta Thunberg");
         names.put("tutta@mail.com", "Tutta Larson");
         for (String key : names.keySet()) {
