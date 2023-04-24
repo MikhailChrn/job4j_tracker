79bb6a8b1298622d7df0c23555d62f7463162054
Mikhail <mikhail.cherneyev@yandex.ru>
2023-04-24 13:27:17 +0400

    1. Паспорт и жители [#212656 #411760] Устранение замечания.

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,4 +11,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenTestAddMethod2() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Mikhail Cherneyev");
+        PassportOffice office = new PassportOffice();
+        assertThat(office.add(citizen1)).isTrue();
+        assertThat(office.add(citizen2)).isFalse();
+    }
 }
