146aac9d9fa66c384b343ba938a74e87b10da2f0
Mikhail <mikhail.cherneyev@yandex.ru>
2023-04-30 13:37:18 +0400

    5. PriorityQueue [#504944 #413156] Delete comments.

--- src/main/java/ru/job4j/queue/TaskByPositionAsc.java
+++ src/main/java/ru/job4j/queue/TaskByPositionAsc.java
@@ -2,9 +2,6 @@ package ru.job4j.queue;
 
 import java.util.Comparator;
 
-//Первый сортирует по полю position по возрастанию,
-// у Enum уже реализован метод compareTo() – именно это и используйте.
-
 public class TaskByPositionAsc implements Comparator<Task> {
     @Override
     public int compare(Task o1, Task o2) {
