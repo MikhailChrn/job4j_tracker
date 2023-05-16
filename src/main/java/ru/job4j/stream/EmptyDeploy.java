9d17c14bcff5ec47f07ea81be372c1e8e59583ae
Mikhail <mikhail.cherneyev@yandex.ru>
2023-05-16 08:04:47 +0400

    4. Перенос методов в Stream API [#279131 #416448] СБОРКА НЕ СОБИРАЕТСЯ

---
+++ src/main/java/ru/job4j/stream/EmptyDeploy.java
@@ -0,0 +1,8 @@
+package ru.job4j.stream;
+
+public class EmptyDeploy  {
+    public static void empty() {
+        System.out.println("СБОРКА НЕ СОБИРАЕТСЯ");
+        System.out.println("https://github.com/MikhailChrn/job4j_tracker/commit/2952ea542abb158267b325f960c9a77374e1a76f");
+    }
+}
