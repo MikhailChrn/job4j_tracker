64a676a8f1d753ba9a796ef87df866b284d086ba
Mikhail <mikhail.cherneyev@yandex.ru>
2023-05-17 08:35:13 +0400

    6. Список адресов [#110062 #416658] Устранение замечания.

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,13 +7,13 @@ import java.util.stream.Collectors;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(p -> p.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
-                .map(p -> p.getAddress())
+                .map(Profile::getAddress)
                 .distinct()
                 .sorted(Comparator.comparing(Address::getCity))
                 .collect(Collectors.toList());
