d42d0603c93c0a01c72a44156def31c3986fb390
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-05-22 07:36:20 +0400

    0. Stream API улучшения [#58167] Корректировка написания аргументов

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -8,8 +8,8 @@ public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
                 .filter(Objects::nonNull)
-                .sorted((st1, st2) ->
-                        Integer.compare(st2.getScore(), st1.getScore()))
+                .sorted((left, right) ->
+                        Integer.compare(right.getScore(), left.getScore()))
                 .takeWhile(st -> st.getScore() > bound)
                 .collect(Collectors.toList());
     }
